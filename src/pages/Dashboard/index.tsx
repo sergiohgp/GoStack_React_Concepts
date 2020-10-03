17bf6766f6e467c7985391aeb79a1c1f45f901af
Sergio Pereira <sergiohgp@gmail.com>
2020-10-03 16:02:16 -0400

    Dashboard interface

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,57 @@
 import React from 'react';
-import { DashboardTitle } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { DashboardTitle, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <DashboardTitle>Explore repositories on Github</DashboardTitle>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <DashboardTitle>Explore repositories on Github</DashboardTitle>
+
+      <Form action="">
+        <input type="text" placeholder="Repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img src="https://avatars0.githubusercontent.com/u/54996062?s=460&u=aaf5a4a3287c87691cab70e3c51ff5ffe7421154&v=4"
+            alt="Sergio Pereira" />
+          <div>
+            <strong>sergiohgp/GoBarber</strong>
+            <p>Easy peasy highly scalable NodeJS & TypeScript application</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img src="https://avatars0.githubusercontent.com/u/54996062?s=460&u=aaf5a4a3287c87691cab70e3c51ff5ffe7421154&v=4"
+            alt="Sergio Pereira" />
+          <div>
+            <strong>sergiohgp/GoBarber</strong>
+            <p>Easy peasy highly scalable NodeJS & TypeScript application</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img src="https://avatars0.githubusercontent.com/u/54996062?s=460&u=aaf5a4a3287c87691cab70e3c51ff5ffe7421154&v=4"
+            alt="Sergio Pereira" />
+          <div>
+            <strong>sergiohgp/GoBarber</strong>
+            <p>Easy peasy highly scalable NodeJS & TypeScript application</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
